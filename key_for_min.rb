4e59c2d02012c0f5c5b969d9fe7202655adcd8e8
Christoph Gruener <christophgruener@me.com>
2019-02-15 02:53:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < low_value
       low_value = value
-      low_key = name_hash[key]
+      low_key = key
     end
   end
   low_key
